8bfaf95e8c403405066f2ff7889f04cee0ce831d
kazishakkhar17 <kazishakkhar04@gmail.com>
2025-04-25 08:07:05 +0600

    Added Notification Trigger

---
+++ src/medi/core/NotificationInfo.java
@@ -0,0 +1,86 @@
+package medi.core;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class NotificationInfo extends JFrame {
+    NotificationInfo(){
+        JPanel panel = new JPanel();
+        panel.setBounds(5, 5, 890, 590);
+        panel.setBackground(new Color(179, 250, 143));
+        panel.setLayout(null);
+        add(panel);
+
+        JTable table = new JTable();
+        table.setBounds(10, 40, 900, 450);
+        table.setBackground(new Color(179, 250, 143));
+        table.setFont(new Font("Tahoma", Font.BOLD, 12));
+        panel.add(table);
+
+        try {
+            conn c = new conn();
+            String q = "SELECT * FROM Notification";
+            ResultSet resultSet = c.statement.executeQuery(q);
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JLabel label1 = new JLabel("Notification ID");
+        label1.setBounds(10, 11, 120, 14);
+        label1.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label1);
+
+        JLabel label2 = new JLabel("Patient Name");
+        label2.setBounds(160, 11, 120, 14);
+        label2.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label2);
+
+        JLabel label3 = new JLabel("Department");
+        label3.setBounds(310, 11, 120, 14);
+        label3.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label3);
+
+        JLabel label4 = new JLabel("Head");
+        label4.setBounds(460, 11, 120, 14);
+        label4.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label4);
+
+        JLabel label5 = new JLabel("Phone");
+        label5.setBounds(610, 11, 120, 14);
+        label5.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label5);
+
+        JLabel label6 = new JLabel("Created At");
+        label6.setBounds(760, 11, 120, 14);
+        label6.setFont(new Font("Tahoma", Font.BOLD, 14));
+        panel.add(label6);
+
+        JButton button = new JButton("BACK");
+        button.setBounds(450, 510, 120, 30);
+        button.setBackground(Color.black);
+        button.setForeground(Color.white);
+        panel.add(button);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
+
+        setUndecorated(true);
+        setSize(900, 600);
+        setLayout(null);
+        setLocation(300, 200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new NotificationInfo();
+    }
+}
